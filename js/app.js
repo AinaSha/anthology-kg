194ed6489d7667ac60e3cba0f9d83d8fa143c29b
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2022-11-21 20:04:44 +0600

    create main page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// *** Burger ***//
+
 const nav = document.querySelector('.nav'),
       burger = document.querySelector('.burger'),
       navItem = document.querySelectorAll('.nav__item');
@@ -16,3 +18,38 @@ const nav = document.querySelector('.nav'),
            burger.classList.remove('active');
         });
       });
+
+
+
+
+ // *** Slide ***//   
+ var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  centerSlide: true,
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+      },
+  },
+});
